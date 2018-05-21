38003d028bbc6bb89b7f3aae721e42d7c8241c07
joens16 <joens16@hotmail.com>
2018-05-20 23:57:41 -0500

    Final touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,29 @@
-// create variables: win, loss, your point, random number picked by pc, variable for each Dragon Ball
+// create variables: win, loss, your points, random number picked by pc, variable for each Dragon Ball
 
-var wins = 0;
+var wins = 1;
 var losses = 0;
 var yourPoints = 0;
 var computerNumber = 0;
+
 var dragonBall1 = 0;
 var dragonBall2 = 0;
 var dragonBall3 = 0;
 var dragonBall4 = 0;
 
+//Generate # 19-120 at random and update computerNumber in HTML
+function generateComputerNumber(){
+    computerNumber = Math.floor(Math.random() * 101) + 19;
+    $("#target").text(computerNumber);
+}
 
+//Reset game and update HTML
+function reset(){
+    yourPoints = 0;
+    $("myScore").text(yourPoints);
+    computerNumber = 0;
+}
 
-// $(document).ready(function(){
-//     $("#targetNumber").on(function() {
-// });
-
-// computer will generate random number for each Dragon Ball each time game resets between 1-12//
+//Generate random #'s for each Dragon Ball (1-12)
 function startGame(){
     
     dragonBall1 = Math.floor(Math.random() * 12) + 1;
@@ -33,79 +41,68 @@ function startGame(){
     dragonBall4 = Math.floor(Math.random() * 12) + 1;
     console.log(dragonBall4);
 
-}
-
-    // computer will generate a random number at the start of the game after win or loss from 19 & 120 //
-function fcomputerNumber(){
-    return computerNumber = Math.floor(Math.random() * 101) + 19;
-    console.log('dentro de la funcion'+computerNumber);
-}
-
-function reset(){
-    yourPoints = 0;
-    console.log('your points'+ yourPoints);
-    computerNumber = fcomputerNumber;
-    console.log('computer'+ computerNumber);
+    reset();
 
 }
 
+//Compare yourPoints w/ computerNumber
 function compareScore(number1, number2){ 
     if (number1 == number2){
-        wins++;
+        wins++; //Add to wins if yourPoints and computerNumber are the same, reset, then call functions 
         $("#myWins").text(wins);
         reset();
         startGame();
+        generateComputerNumber();
         alert('You won');
-    }else if(number1 < number2){
+    }else if(number1 > number2){
         losses++;
         $("#myLosses").text(losses);
         reset();
         startGame();
+        generateComputerNumber();
         alert('You lost');
     }
 }
 
 $(document).ready(function() {
     
+    //Calling to generate #'s for dragon Balls and computer number
     startGame();
-    computerNumber = fcomputerNumber();
+    generateComputerNumber();
 
-    $("#target").text(computerNumber);
+  
 
 //When click on ball, it adds to your total score
     $("#ballscontainer").on("click", "#ball1", function() {
     yourPoints += dragonBall1;
-    compareScore(computerNumber, yourPoints);
-    console.log(yourPoints);
-    $("#myScore").text(yourPoints)
-    }).on("click", "#ball2", function() {
+        
+        //HTML -updating 
+        $("#myScore").text(yourPoints);
+        compareScore(yourPoints, computerNumber);//calling function to compare values
+    });
+
+    $("#ballscontainer").on("click", "#ball2", function() {
     yourPoints += dragonBall2;
-    compareScore(computerNumber, yourPoints);
-    console.log(yourPoints);
-    $("#myScore").text(yourPoints)
-    }).on("click", "#ball3", function() {
+
+        $("#myScore").text(yourPoints);
+        compareScore(yourPoints, computerNumber);//calling function to compare values
+    });
+
+    $("#ballscontainer").on("click", "#ball3", function() {
     yourPoints += dragonBall3;
-    compareScore(computerNumber, yourPoints);
-    console.log(yourPoints);
-    $("#myScore").text(yourPoints)
-    }).on("click", "#ball4", function() {
+
+        $("#myScore").text(yourPoints);
+        compareScore(yourPoints, computerNumber);//calling function to compare values
+    });
+
+    $("#ballscontainer").on("click", "#ball4", function() {
     yourPoints += dragonBall4;
-    compareScore(computerNumber, yourPoints);
-    console.log(yourPoints);
-    $("#myScore").text(yourPoints)
+
+        $("#myScore").text(yourPoints);
+        compareScore(yourPoints, computerNumber);//calling function to compare values
     });
 });
-
-
-
-
-
-
-// Your game will hide this amount until the player clicks a crystal
-
-//If your points match the computer generated random number, you win. adds to win variable
-
-//if your points go over the number the computer generated, you lose, adds to loss variable
+   
 
 
 
